a41f474e31c77930c515119b8be978c36dbb6999
raphael-foliveira <raphael.foliveira1@gmail.com>
2022-04-21 18:51:44 -0300

    lift-state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,25 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 
 function Expenses(props){
 
-    return (
-        <Card className="expenses">
+    const [filterYear, setFilteredYear] = useState("2022")
 
+    const filterChangeHandler = (selectedYear) => {
+        console.log("Expenses.js");
+        setFilteredYear(selectedYear);
+    }
+
+    return (
+        <div>
+            
+
+        <Card className="expenses">
+        <ExpensesFilter currentlySelected={filterYear} selectedYear={filterChangeHandler}/>
         {props.data.map(expense => (
             <ExpenseItem 
             title={expense.title}
@@ -16,6 +28,7 @@ function Expenses(props){
             ></ExpenseItem>
             ))}
         </Card>
+            </div>
       )
 }
 
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,21 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+
+    const expenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+
+        props.onSaveExpense(enteredExpenseData);
+        return expenseData;
+    };
+
     return (
         <div className="new-expense">
-            <ExpenseForm/>
+            <ExpenseForm onSaveExpenseData={expenseDataHandler}/>
         </div>
     );
 }
